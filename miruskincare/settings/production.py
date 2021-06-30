ba20dd3d6d0b50fff9b1270f70a97506184fdc5b
Ider Badamdorj <ider@Iders-MacBook-Air.local>
2021-06-30 18:18:35 +0800

    added allowed host locacl

--- miruskincare/settings/production.py
+++ miruskincare/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ['ip-address', 'ider0514.pythonanywhere.com']
+ALLOWED_HOSTS = ['ip-address', 'ider0514.pythonanywhere.com', '127.0.0.1']
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
