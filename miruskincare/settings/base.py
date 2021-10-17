0bdfd67fb55e8a7313ee600ec79c559417004876
ider514 <ider0514@gmail.com>
2021-10-17 16:25:28 +0800

    disabled fb login

--- miruskincare/settings/base.py
+++ miruskincare/settings/base.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'core',
 
-    'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.facebook',
 ]
 
 MIDDLEWARE = [
